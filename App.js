9dd781822115e81409e76f6169d420f238a8baad
Lance Judan <aperture.phoenix@gmail.com>
2019-09-12 15:29:33 -0500

    Capitalization saves lives. Added basic navigation

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import MyShoppingList from './views/MyShoppingList'
+import Login from './views/Login'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const appNavigator = createStackNavigator({
+  Login: { screen: Login},
+  Home: { screen: MyShoppingList}
+}, { initialRouteName: 'Login'} )
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default createAppContainer(appNavigator)
---
+++ views/MyShoppingList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class MyShoppingList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>This is My Shopping List</Text>
+            </View>
+        )
+    }
+}
+
+export default MyShoppingList
