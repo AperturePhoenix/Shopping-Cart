8d095d0ab6eb0cc31ed74fffeaa05c1f8d0cbcaf
Lance Judan <aperture.phoenix@gmail.com>
2019-09-13 16:21:30 -0500

    Sorry sir, I'm gonna have to check your ID. Added Login

--- views/Login.js
+++ views/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, AsyncStorage } from 'react-native'
+import { View, AsyncStorage, Alert } from 'react-native'
 import { Button, Input } from 'react-native-elements'
 import firebase from 'firebase'
 import base64 from 'react-native-base64'
@@ -12,39 +12,65 @@ class Login extends Component {
     constructor(props) {
         super(props)
 
-        thisdb = firebase.firestore().collection('users')
+        this.db = firebase.firestore().collection('users')
         this.state = {
-            username: '',
-            password: ''
+            username: '', usernameError: '',
+            password: '', passwordError: ''
         }
     }
 
     render() {
         return(
             <View>
-                <Input placeholder='Username' onChangeText={ username => this.setState({ username: username })} />
-                <Input placeholder='Password' onChangeText={ password => this.setState({ password: password })} secureTextEntry={true} />
-                <Button title='Sign In' onPress={ this._logIn } />
+                <Input placeholder='Username' onChangeText={ username => this.setState({ username: username })} errorStyle={{ color: 'red' }} errorMessage={this.state.usernameError} />
+                <Input placeholder='Password' onChangeText={ password => this.setState({ password: password })} secureTextEntry={true} errorStyle={{ color: 'red' }} errorMessage={this.state.passwordError} />
+                <Button title='Sign In' onPress={ this.logIn } />
                 <Button title='Register' onPress={ () => this.props.navigation.navigate('Register') } />
             </View>
         )
     }
 
-    _logIn = () => {
-        this._setLoginData()
-        this.props.navigation.navigate('App')
+    validateInformation = () => {
+        isValid = true
+        if (!this.state.username) {
+            this.setState({
+                usernameError: 'Please enter a username'
+            })
+            isValid = false
+        } else { this.setState({
+            usernameError: ''
+        })}
+        if (!this.state.password) {
+            this.setState({
+                passwordError: 'Please enter a password'
+            })
+            isValid = false
+        } else { this.setState({
+            passwordError: ''
+        })}
+
+        return isValid
+    }
+
+    logIn = () => {
+        if (this.validateInformation()) {
+            this.db.doc(this.state.username).get()
+                .then(user => {
+                    if (user.exists && user.get('password') == base64.encode(this.state.password)) {
+                        this._setLoginData()
+                        this.props.navigation.navigate('App')
+                    } else {
+                        Alert.alert( message='Invalid username or password' )
+                    }
+                })
+        }
     }
 
     _setLoginData = async() => {
         await AsyncStorage.setItem('username', this.state.username)
+        await AsyncStorage.setItem('name', this.db.doc(this.state.username).get('name'))
         await AsyncStorage.setItem('password', base64.encode(this.state.password))
     }
-
-    //TODO: Log out
-    // _signOutAsync = async () => {
-    //     await AsyncStorage.clear();
-    //     this.props.navigation.navigate('Auth');
-    //   };
 }
 
 export default Login
